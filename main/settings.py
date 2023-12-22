50d2453a652ca40734baa8b0be825cb880d386e4
Daniel-March <town.kor@gmail.com>
2023-12-23 00:41:23 +0300

    DEBUG disabled

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 if (docker_service_hostname := os.getenv("HOSTNAME")) is not None:
